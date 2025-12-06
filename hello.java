17db0b91ba4f9740e610d2d799afe080f4ea0180
girijeshhs <girijeshhs@gmail.com>
2025-12-06 10:26:12 +0530

    initial commit

---
+++ hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class ReverseInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+
+        int n = sc.nextInt();
+        int k = n;
+        int rev = 0;
+
+        for (i=0;i<n;i++){
+
+        }
+    }
+}
