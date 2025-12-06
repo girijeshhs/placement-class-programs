8f131fa0f0819d10a1af8a065b98b9e33dec83d0
girijeshhs <girijeshhs@gmail.com>
2025-12-06 12:25:44 +0530

    using string

--- frequency.java
+++ frequency.java
@@ -1,11 +1,10 @@
 public class frequency{
     public static void main(String[] args) {
         String str = "banana";
-        char[] as = str.toCharArray(); //
         char target = 'a';
         int count = 0;
-        for(int i =0;i<as.length;i++) {
-            if(as[i] == target){
+        for(int i =0;i<str.length();i++) {
+            if(str.charAt(i) == target){
                 count++;
             }
         }
